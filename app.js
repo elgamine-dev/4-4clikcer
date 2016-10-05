d4e715e50a0f621a65488acc1df8863ff12cbf1d
Fred <delon.frederic.pro.job@gmail.com>
2016-10-05 10:47:01 +0200

    Fred end commit 4:4 clicker

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 $(document).ready(function(){
 	var i = 0;
+	 //var rand = (Math.random() * tab);
 	$('img').click(function(){
 		$("#lol").text(++i); 
 		if ( i % 20 === 0){
-			alert("TDADAAAAAAAAA")
+	var tab = ["salut","yola","Ez bro", "learn to play noob", "learn to code","foo"];
+	var rand = tab[Math.floor(Math.random()*tab.length)];
+				alert(rand)
+			console.log(rand);
 			}
 		
 
@@ -11,5 +15,3 @@ $(document).ready(function(){
 	
 	
 });
-
-var arr = new array["YO","PE","Perdu","gagné"]
