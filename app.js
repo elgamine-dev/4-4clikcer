c39a7e77ce316872a6221a37dcb7cbcbd87f1219
Fred <delon.frederic.pro.job@gmail.com>
2016-10-07 10:59:25 +0200

    Fred end commit

--- app.js
+++ app.js
@@ -1,17 +1,11 @@
 $(document).ready(function(){
 	var i = 0;
-	 //var rand = (Math.random() * tab);
 	$('img').click(function(){
 		$("#lol").text(++i); 
 		if ( i % 20 === 0){
 	var tab = ["salut","yola","Ez bro", "learn to play noob", "learn to code","foo"];
 	var rand = tab[Math.floor(Math.random()*tab.length)];
 				alert(rand)
-			console.log(rand);
 			}
-		
-
 	});
-	
-	
 });
